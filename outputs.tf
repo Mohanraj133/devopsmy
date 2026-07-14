499b0c0499b7ac423364faa1011b7a8119c767a6
Mohanraj Perumal <mohanaws070423@gmail.com>
2026-07-14 07:54:32 +0530

    security group and keypair updated

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,17 @@
 output "instance_id" {
-value = aws_instance.web.id
+value = aws_instance.terraform.id
   
 }
 output "public_ip" {
-    value = aws_instance.web.public_ip
+    value = aws_instance.terraform.public_ip
+  
+}
+output "key_pair" {
+    value = aws_instance.terraform.key_name
+  
+}
+
+output "security_group" {
+    value = aws_instance.terraform.vpc_security_group_ids
   
 }

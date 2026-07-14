fbfc04de8520ead6c7c17816d292623a81b67dae
Mohanraj Perumal <mohanaws070423@gmail.com>
2026-07-14 06:33:28 +0530

    firstsamplecode

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+    type = string
+    default = "ap-south-1"
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.small"
+  
+}
+
+variable "ami" {
+    type = string
+    default = "ami-0b910d1016287a5e7"
+  
+}
